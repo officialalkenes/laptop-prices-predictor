0d113b124e2b769ffec7e114f91b2843ca5374b7
alkenes <belloabdulhakeemolamide@gmail.com>
2023-04-27 11:15:28 +0100

    chore: README.md file updated

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Laptop Price Predictor
 
+
+## Prerequisites
+Ensure you have a code editor installed (preferrably [`VS code`]("https://code.visualstudio.com/")). you need to have Python installed on your computer. You can download Python from the [official website]('https://www.python.org/downloads/').
+
+## Installation
+To install Streamlit, you can use the pip package manager. Here are the steps:
+
+- Download the folder.
+- Open folder with your command line tool
+- 
+- Type pip install streamlit (without the quotes) and press Enter.
+- Wait for the installation to complete.
+That's it! You have successfully installed Streamlit on your computer.
+
+Running Streamlit
+To run Streamlit, you can use the command line interface (CLI). Here are the steps:
+
+Open a command prompt or terminal window.
+Navigate to the directory where your Streamlit app is located.
+Type streamlit run app.py (replace app.py with the name of your app file) and press Enter.
+Wait for Streamlit to start.
+Open your web browser and navigate to http://localhost:8501.
+That's it! You should now see your Streamlit app running in your web browser.
+
+Conclusion
+This guide explained how to install Streamlit for absolute beginners. If you encounter any issues during the installation process, feel free to consult the official Streamlit documentation or ask for help on the Streamlit community forum.
+
+
+
+
+
+
--- st-predictor.py
+++ st-predictor.py
@@ -1,8 +1,94 @@
 import pickle
 
 import numpy as np
+import pandas as pd
 import streamlit as st
 
-
-# Load Processed model
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
+st.title("Laptop Price Prediction App")
+st.markdown("""
+This app Estimates the prices of your Laptops based on preferred specification
+            """)
+
+
+@st.cache_data
+def load_data(filename):
+    return pd.read_csv(filename)
+
+filename = 'testing.csv'
+df = load_data(filename)
+
+
+
+company = df.groupby('Company')
+type_name = df.groupby("TypeName")
+ram = df.groupby("Ram")
+weight = df.groupby("Weight")
+touchscreen = df.groupby("TouchScreen")
+ips = df.groupby("IPS")
+ppi = df.groupby("PPI")
+cpu_brand = df.groupby("Cpu_brand")
+hdd = df.groupby("HDD")
+ssd = df.groupby("SSD")
+gpu_brand = df.groupby("Gpu brand")
+os = df.groupby("os")
+
+
+
+@st.cache_data
+def predict(value_arrays):
+    numpy_array = np.asarray(value_arrays)
+    reshaped_array = numpy_array.reshape(1, -1)
+    prediction = loaded_model.predict(reshaped_array)
+    st.write(f'based on your inputs Your preferred Laptop price is estimated at {str(prediction)} Euros.')
+
+# Sidebar - Company selection
+sorted_company = sorted( df['Company'].unique())
+sorted_ram = sorted( df['Ram'].unique())
+sorted_type_name = sorted( df['TypeName'].unique())
+sorted_weight = sorted(df['Weight'].unique())
+sorted_touch = sorted(df['TouchScreen'].unique())
+sorted_ips = sorted(df['IPS'].unique())
+sorted_ppi = sorted(df['PPI'].unique())
+sorted_cpu = sorted(df['Cpu_brand'].unique())
+sorted_hdd = sorted(df['HDD'].unique())
+sorted_ssd = sorted(df['SSD'].unique())
+sorted_gpu = sorted(df['Gpu brand'].unique())
+sorted_os = sorted(df['os'].unique())
+
+os_sel = st.selectbox('Choose Preferred Operating Software', sorted_os)
+company_sel = st.selectbox('Pick Company', sorted_company)
+ram_sel = st.selectbox('Select Ram (GB)', sorted_ram)
+type_sel = st.selectbox('Select Laptop Type', sorted_type_name)
+weight_sel = st.selectbox('Preferred Weight (KG)', sorted_weight)
+touch_sel = st.selectbox('Touch Screen (1=Yes, 0=No)', sorted_touch)
+hdd_sel = st.selectbox('HDD Memory (GB)', sorted_hdd)
+ssd_sel = st.selectbox('SSD Memory (GB)', sorted_ssd)
+# touch_sel = st.selectbox('Touch Screen (1=Yes, 0=No)', sorted_touch)
+ips_sel = st.selectbox('IPS (1=Yes, 0=No)', sorted_ips)
+ppi_sel = st.selectbox('Pixel Per Inch', sorted_ppi)
+gpu_sel = st.selectbox('Select GPU Brand', sorted_gpu)
+cpu_sel = st.selectbox('Choose CPU Brand', sorted_cpu)
+
+
+if st.button("Predict Now"):
+    selections = [company_sel, type_sel, ram_sel, weight_sel, touch_sel, ips_sel, ppi_sel, cpu_sel, hdd_sel, ssd_sel, gpu_sel,  os_sel]
+    predictions = predict(selections)
+
+# 	Company	TypeName	Ram	Weight	TouchScreen	IPS	PPI	Cpu_brand	HDD	SSD	Gpu brand	os
+# Sidebar - Company selection
+# sorted_company_unique = sorted( df['GICS Sector'].unique() )
+# selected_sector = st.sidebar.select('Sector', sorted_company_unique, sorted_company_unique)
+
+
+# # Sidebar - Company selection
+# sorted_company_unique = sorted( df['GICS Sector'].unique() )
+# selected_sector = st.sidebar.select('Sector', sorted_company_unique, sorted_company_unique)
+
+
+# # Sidebar - Company selection
+# sorted_company_unique = sorted( df['GICS Sector'].unique() )
+# selected_sector = st.sidebar.select('Sector', sorted_company_unique, sorted_company_unique)
+
+
